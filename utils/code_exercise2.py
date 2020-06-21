9e52f86293e7ff83200cdc1b3abb4d72424a990b
Christoper <christoper@go-jek.com>
2020-06-21 16:03:19 +0200

    ex 5

--- utils/code_exercise2.py
+++ utils/code_exercise2.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+from utils.code_exercise1 import my_window
+import matplotlib.pyplot as plt
 
 def convert_to_samples(milliseconds: int, sampling_freq: int):
     """
@@ -42,3 +43,27 @@ def compute_istft(stft: np.ndarray, sampling_rate: int, frame_shift: int, synthe
         time_signal[i*samples_per_shift:i*samples_per_shift+samples_per_frame] += time_frames[i]
 
     return time_signal
+
+def compute_stft(v_signal: np.ndarray, sampling_rate: int, frame_length: int, frame_shift: int, v_analysis_window: np.ndarray):
+    segments, v_time = my_window(v_signal, sampling_rate, frame_length, frame_shift)
+
+    seg_windows = segments * v_analysis_window
+
+    n_length = int(sampling_rate * frame_length / 1000)
+    freqs = np.fft.fftfreq(n_length)
+
+    m_stft = np.fft.fft(seg_windows)[:,:(n_length//2 +1)] # consider only lower half of the spectrum
+
+    v_freq = freqs[:n_length//2] * sampling_rate
+
+    return m_stft, v_freq, v_time
+
+
+def plot_spectrum(m_stft, v_freq, v_time):
+    fig = plt.figure(figsize=(20,10))
+    ax = fig.add_subplot(111)
+
+    im = ax.imshow(10* np.log10(np.maximum(np.square(np.abs(m_stft.T)), 10**( -15))),
+                    cmap ='viridis', origin ='lower', extent =[ v_time [0] , v_time [-1] , v_freq [0] ,
+                    v_freq [ -1]] , aspect ='auto')
+    fig.colorbar(im , orientation ="vertical", pad =0.2)
